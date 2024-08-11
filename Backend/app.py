2234725360807f7a8f65ca25591bbc4052d50a27
Sergayka <xuver@yandex.ru>
2024-08-12 02:48:56 +0300

    222

--- Backend/app.py
+++ Backend/app.py
@@ -57,4 +57,4 @@ def get_all_collectionsName():
         return jsonify({'error': 'Ошибка при получении данных'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
